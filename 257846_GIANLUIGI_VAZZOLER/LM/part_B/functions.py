7d8b8ef2232ca3f3a6936945d484d1f619289d93
gvazzolerunitn <gianluigi.vazzoler@studenti.unitn.it>
2025-05-20 17:07:45 +0200

    fixed wrong naming

--- 257846_GIANLUIGI_VAZZOLER/LM/part_B/functions.py
+++ 257846_GIANLUIGI_VAZZOLER/LM/part_B/functions.py
@@ -1,73 +1,90 @@
-# Add the class of your model only
-# Here is where you define the architecture of your model using pytorch
-
+import numpy as np
+import torch
 import torch.nn as nn
+import torch.optim as optim
+import math
+import matplotlib.pyplot as plt
+import copy
+
+from tqdm import tqdm
+
+from model import *
+from utils import *
+
+def train_loop(data, optimizer, criterion, model, clip=5):
+    model.train()
+    loss_array = []
+    number_of_tokens = []
     
-# CODE BEFORE VARIATIONAL DROPOUT    
-""" class LM_LSTM(nn.Module):
-    def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.1,
-                 emb_dropout=0.1, n_layers=1):
-        assert emb_size == hidden_size
-        super(LM_LSTM, self).__init__()
-        # Token ids to vectors, we will better see this in the next lab 
-        self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
-
-        # Pytorch's RNN layer: https://pytorch.org/docs/stable/generated/torch.nn.RNN.html
-        self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)    
-        self.pad_token = pad_index
-
-        # Linear layer to project the hidden layer to our output space 
-        self.output = nn.Linear(hidden_size, output_size)
-        self.output.weight = self.embedding.weight
+    for sample in data:
+        optimizer.zero_grad() # Zeroing the gradient
+        output = model(sample['source'])
+        loss = criterion(output, sample['target'])
+        loss_array.append(loss.item() * sample["number_tokens"])
+        number_of_tokens.append(sample["number_tokens"])
+        loss.backward() # Compute the gradient, deleting the computational graph
+        # clip the gradient to avoid exploding gradients
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)  
+        optimizer.step() # Update the weights
         
-    def forward(self, input_sequence):
-        emb = self.embedding(input_sequence)
+    return sum(loss_array)/sum(number_of_tokens)
 
-        lstm_out, _ = self.lstm(emb)
+def eval_loop(data, eval_criterion, model):
+    model.eval()
+    loss_to_return = []
+    loss_array = []
+    number_of_tokens = []
+    # softmax = nn.Softmax(dim=1) # Use Softmax if you need the actual probability
+    with torch.no_grad(): # It used to avoid the creation of computational graph
+        for sample in data:
+            output = model(sample['source'])
+            loss = eval_criterion(output, sample['target'])
+            loss_array.append(loss.item())
+            number_of_tokens.append(sample["number_tokens"])
+            
+    ppl = math.exp(sum(loss_array) / sum(number_of_tokens))
+    loss_to_return = sum(loss_array) / sum(number_of_tokens)
+    return ppl, loss_to_return
 
-        output = self.output(lstm_out).permute(0,2,1)
-        return output """
+def init_weights(mat):
+    for m in mat.modules():
+        if type(m) in [nn.GRU, nn.LSTM, nn.RNN]:
+            for name, param in m.named_parameters():
+                if 'weight_ih' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.xavier_uniform_(param[idx*mul:(idx+1)*mul])
+                elif 'weight_hh' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.orthogonal_(param[idx*mul:(idx+1)*mul])
+                elif 'bias' in name:
+                    param.data.fill_(0)
+        else:
+            if type(m) in [nn.Linear]:
+                torch.nn.init.uniform_(m.weight, -0.01, 0.01)
+                if m.bias != None:
+                    m.bias.data.fill_(0.01)
 
-# DROPOUT CLASS
-class LockedDropout(nn.Module):
-    def __init__(self):
-        super(LockedDropout, self).__init__()
+# Experiment also with a smaller or bigger model by changing hid and emb sizes 
+# A large model tends to overfit
+hid_size = 300 # OLD: 200 (they must be the same value for weight tying)
+emb_size = 300
 
-    def forward(self, x, dropout=0.5):
-        if not self.training or dropout == 0:
-            return x
-        mask = x.new_empty(1, x.size(1), x.size(2)).bernoulli_(1 - dropout)
-        mask = mask.div_(1 - dropout)
-        mask = mask.expand_as(x)
-        return x * mask
+# Don't forget to experiment with a lower training batch size
+# Increasing the back propagation steps can be seen as a regularization step
 
-# CODE WITH VARIATIONAL DROPOUT
-class LM_LSTM(nn.Module):
-    def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.1,
-                 emb_dropout=0.1, n_layers=1):
-        assert emb_size == hidden_size
-        super(LM_LSTM, self).__init__()
+# With SGD try with an higher learning rate (> 1 for instance)
+lr = 1 # This is definitely not good for SGD [try 1 for SGD and 0.001 for AdamW]
+clip = 5 # Clip the gradient
 
-        self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
+vocab_len = len(lang.word2id)
 
-        self.input_locked_dropout = LockedDropout()
-        self.output_locked_dropout = LockedDropout()
+model = LM_LSTM(emb_size, hid_size, vocab_len, pad_index=lang.word2id["<pad>"]).to(DEVICE)
+model.apply(init_weights)
 
-        self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)
-        self.pad_token = pad_index
+# OLD CODE => SGD
+optimizer = optim.SGD(model.parameters(), lr=lr)
 
-        self.output = nn.Linear(hidden_size, output_size)
-        self.output.weight = self.embedding.weight  # weight tying
-
-        self.emb_dropout_p = emb_dropout
-        self.out_dropout_p = out_dropout
-
-    def forward(self, input_sequence):
-        emb = self.embedding(input_sequence)
-        emb = self.input_locked_dropout(emb, dropout=self.emb_dropout_p)
-
-        lstm_out, _ = self.lstm(emb)
-        lstm_out = self.output_locked_dropout(lstm_out, dropout=self.out_dropout_p)
-
-        output = self.output(lstm_out).permute(0, 2, 1)
-        return output
+criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
+criterion_eval = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"], reduction='sum')
